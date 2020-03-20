4ea1e71c377d93f4c0b2acb18b1e6ddbc109ea14
N-Tomomi <d_o_yukijiro@hotmail.co.jp>
2020-03-20 14:18:17 +0900

    game1_game2

---
+++ public/js/game1.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('#inu-icon-game1').fadeIn(1700);
+  $('#inu-icon-game1').on('click', function(){
+    $('#inu-icon-game1').css('left','50px');
+    $('#hidden-message-to-game2').show();
+  });
+
+});
---
+++ public/js/game2.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('#inu-icon-game2').fadeIn(1700);
+  $('#inu-icon-game2').on('click', function(){
+    $('#inu-icon-game2').css('left','50px');
+    $('#hidden-message-to-game1').show();
+  });
+
+});
